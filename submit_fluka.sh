7d97ca902494efb9980bfc191cb11beafefb6d37
Lorenzo Zana <lorenzozana@gmail.com>
2018-09-28 15:03:08 -0400

    Final tuning for the submitting script

--- submit_fluka.sh
+++ submit_fluka.sh
@@ -3,6 +3,7 @@ file="beam_time.txt"
 hconv=720 
 i=0
 run_per_conf=20
+sub=0
 while read line ; do
     
     if [ $i -gt 0 ] 
@@ -13,11 +14,7 @@ while read line ; do
 	target=`echo $target |  sed 's/ /*/g'`
 	hours=`echo $line | awk '{printf("%s \n",$3)}'`
 	hours=`echo $hours |  sed 's/ /*/g'`
-	seconds=`awk -vp=$hours -vq=$hconv 'BEGIN{printf "%d" ,p * q}'`
-	if [ "${#seconds}" -eq "4" ]
-	    then
-	    seconds=`echo "  " $seconds`
-	fi
+	seconds=`awk -vp=$hours -vq=$hconv 'BEGIN{printf "%6d" ,p * q}'`	
 	# seconds divided by 5 (3600/5) since I will consider 5 different time of irradiation followed by 4 of waiting beam
 	current=`echo $line | awk '{printf("%s \n",$4)}'`
 	current=`echo $current |  sed 's/ /*/g'`
@@ -25,9 +22,28 @@ while read line ; do
 	# need to modify the current in particle per second
 	energy=`echo $line | awk '{printf("%s \n",$5)}'`
 	energy=`echo $energy |  sed 's/ /*/g'`
+	energy=`awk -vp=$energy 'BEGIN{printf "%4s",p}'`
 	echo "Conf n.="$conf_n "target="$target " seconds="$seconds "current="$current " energy="$energy
-#	perl -pe "s/.*/IRRPROFI     604800.   $current   345600.       0.0   604800.   $current/ if $. == 1056" < hallC_beampipe_${target}.inp >  hallC_beampipe_${target}_${conf_n}.inp
-	
+	perl -pe "s/.*/IRRPROFI     $seconds.  $current   $seconds.       0.0   $seconds.  $current/ if $. == 1056 ; s/.*/IRRPROFI     $seconds.  $current   $seconds.       0.0   $seconds.  $current/ if $. == 1062 ; s/.*/IRRPROFI     $seconds.       0.0   $seconds.  $current   $seconds.       0.0/ if $. == 1059 ; s/.*/BEAM           -$energy             .142857      0.01      0.01          ELECTRON/ if $. == 6" < hallC_beampipe_${target}.inp >  hallC_beampipe_${target}_${conf_n}.inp
+	j=0
+	while [ $j -le ${run_per_conf} ] 
+	do
+	    rn=`perl -e 'my $minimum = 1E8 ; my $range = 9E7 ; my $random_number = int(rand($range)) + $minimum ; print $random_number '`
+	    perl -pe "s/.*/RANDOMIZ          1.$rn./ if $. == 9" < hallC_beampipe_${target}_${conf_n}.inp > hallC_beampipe_${target}_${conf_n}_${j}.inp 
+	    echo "PROJECT: radcon" > farmrun_radcon_hall${conf_n}_${j}.jsub 
+	    echo "TRACK: simulation" >> farmrun_radcon_hall${conf_n}_${j}.jsub
+	    echo "JOBNAME: HallC_beampipe"${conf_n} >> farmrun_radcon_hall${conf_n}_${j}.jsub
+	    echo "COMMAND: ~/Hall-C/run_fluka_multiple.sh" >> farmrun_radcon_hall${conf_n}_${j}.jsub
+	    echo "MEMORY: 2000 MB" >> farmrun_radcon_hall${conf_n}_${j}.jsub                        
+	    echo "OS: centos7" >> farmrun_radcon_hall${conf_n}_${j}.jsub                            
+	    echo "INPUT_FILES: ~/Hall-C/hallC_beampipe_"${target}"_"${conf_n}"_"${j}".inp" >> farmrun_radcon_hall${conf_n}_${j}.jsub
+	    if [ ${sub} -eq "1" ]                                                                               
+            then
+		echo "Submitting job Conf n." $conf_n " n." $j
+		jsub farmrun_radcon_hall${conf_n}_${j}.jsub
+	    fi
+	    ((j=j+1))
+	done
     fi
     ((i=i+1)) 
 done < $file 
